a9ab899fb99e690951330ffc40040bbc43dfa45d
Mike W <mwc1.0@hotmail.com>
2017-10-18 09:52:09 -0500

    Fixing minor error in onehot transformer

--- R/rbm.R
+++ R/rbm.R
@@ -3,7 +3,7 @@ if(exists("configs") == F){
 	library(deepnet)
 	configs = data.frame(hidden_rbm=integer(), numempochs_rbm=integer(), batchsize_rbm=integer(), lr_rbm=numeric(), cd=integer(), hidden_nn=character(), lr_nn=numeric(), numepochs_nn=integer(), batchsize_nn=numeric(), onehot=integer(), err_score=numeric(), err_score_norm=numeric())
 	predict_list <- list()
-	predict_norm <- list()
+	predict_norm_list <- list()
 	nn_list <- list()
 }
 onehot <- 1
@@ -11,44 +11,44 @@ onehot <- 1
 hidden_rbm <- 100
 numepochs_rbm <- 10
 batchsize_rbm <- 100
-learningrate_rbm <- 0.01
+learningrate_rbm <- 0.001
 learningrate_scale_rbm <- 0.9
 cd <- 10
 
 hidden_nn <- c(10)
-learningrate_nn <- 0.01
+learningrate_nn <- 0.001
 learningrate_scale_nn <- 0.9
 numepochs_nn <- 10
 batchsize_nn <- 100
 
 X_train <- read.csv(file="../X_train.csv", header=T, sep=",", row.names=1)
 X_test <- read.csv(file="../X_test.csv", header=T, sep=",", row.names=1)
-y_train <- read.csv(file="../y_train.csv", header=T, sep=",", row.names=1)
-y_test <- read.csv(file="../y_test.csv", header=T, sep=",", row.names=1)
+y_train_org <- read.csv(file="../y_train.csv", header=T, sep=",", row.names=1)
+y_test_org <- read.csv(file="../y_test.csv", header=T, sep=",", row.names=1)
 
 X_train <- X_train[1:939,]
 X_test <- X_test[1:313,]
 
 # trying one hot encoding
-onehot_test <- matrix(0L, nrow=dim(y_test)[1], ncol=max(y_test)+1)
+onehot_test <- matrix(0L, nrow=dim(y_test_org)[1], ncol=max(y_test_org)+1)
 counter <- 1
-for(y in 1:dim(y_test)[1]){
-	onehot_test[counter,y_test[y,]] <- 1
+for(y in 1:dim(y_test_org)[1]){
+	onehot_test[counter,y_test_org[y,]+1] <- 1
 	counter <- counter+1
 }
 # trying one hot encoding
-onehot_train <- matrix(0L, nrow=dim(y_train)[1], ncol=max(y_train)+1)
+onehot_train <- matrix(0L, nrow=dim(y_train_org)[1], ncol=max(y_train_org)+1)
 counter <- 1
-for(y in 1:dim(y_train)[1]){
-	onehot_train[counter, y_train[y,]+1] <- 1
+for(y in 1:dim(y_train_org)[1]){
+	onehot_train[counter, y_train_org[y,]+1] <- 1
 	counter <- counter+1
 }
 
 
 X_train <- as.matrix(X_train)
 X_test <- as.matrix(X_test)
-y_train <- as.matrix(y_train)
-y_test <- as.matrix(y_test)
+y_train <- as.matrix(y_train_org)
+y_test <- as.matrix(y_test_org)
 
 if(onehot){
 	y_test <- onehot_test
@@ -82,7 +82,7 @@ score_norm <- 1- score_norm/dim(pred)[1]
 
 configs <- rbind(configs,data.frame(hidden_rbm=hidden_rbm, numempochs_rbm = numepochs_rbm, batchsize_rbm=batchsize_rbm, lr_rbm=learningrate_rbm, cd=cd, hidden_nn=paste(hidden_nn,collapse=" "), lr_nn=learningrate_nn, numepochs_nn=numepochs_nn, batchsize_nn=batchsize_nn, onehot=onehot, err_score=score, err_score_norm = score_norm))
 predict_list[[dim(configs)[1]]] <- pred
-predict_norm[[dim(configs)[1]]] <- pred_norm
+predict_norm_list[[dim(configs)[1]]] <- pred_norm
 nn_list[[dim(configs)[1]]] <- nn
 
 configs
