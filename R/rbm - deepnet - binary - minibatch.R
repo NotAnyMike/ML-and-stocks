fba905ad9bf89552e37dde7f34514dc1e19e04d2
Mike W <mwc1.0@hotmail.com>
2018-02-05 16:59:02 -0500

    adding autoencoders mnist

--- R/rbm - deepnet - binary - minibatch.R
+++ R/rbm - deepnet - binary - minibatch.R
@@ -3,7 +3,7 @@ if(exists("configs") == F){
 	library(deepnet)
 	
 	#Creating list to store the differents outputs and information
-	configs = data.frame(hidden_rbm=integer(), numempochs_rbm=integer(), batchsize_rbm=integer(), lr_rbm=numeric(), lr_scale_rbm=numeric(), cd=integer(), hidden_nn=character(), lr_nn=numeric(),lr_scale_nn=numeric(), numepochs_nn=integer(), batchsize_nn=numeric(), using_rbm=logical(), err_score=numeric(), err_score_norm=numeric())
+	configs = data.frame(hidden_rbm=integer(), numempochs_rbm=integer(), batchsize_rbm=integer(), lr_rbm=numeric(), lr_scale_rbm=numeric(), cd=integer(), hidden_nn=character(), lr_nn=numeric(),lr_scale_nn=numeric(), numepochs_nn=integer(), batchsize_nn=numeric(), using_rbm=logical(), err_score_norm=numeric())
 	predict_list <- list()
 	predict_norm_list <- list()
 	nn_list <- list()
@@ -13,7 +13,7 @@ if(exists("configs") == F){
 OneClassOutput <- T
 UseRandomSampling <- F
 use_rbm <- T
-pseudominibatch <- 700
+pseudominibatch <- 100
 
 #Hyperparameters for the rbm
 hidden_rbm <- 50
@@ -111,22 +111,22 @@ for(starting in 1:(nrow(X)-pseudominibatch-1)){
 }
 
 #Calculating the score normalized
-pred <- nn.predict(nn, X_test)
 pred_norm <- matrix(0L, nrow=dim(pred)[1], ncol=dim(pred)[2])
+pred_norm[,2] <- pred[,2]
 for(i in 1:dim(pred)[1]){
-	max_row <- which.max(pred[i,])
-	pred_norm[i,max_row] <- 1
+	if(pred[i,1] >= 0.5) pred_norm[i,1] <- 1
+	else pred_norm[i,1] <- 0
 }
 score_norm <- 0
 for(i in 1:dim(pred_norm)[1]){
-	if(which.max(pred_norm[i,]) == which.max(y_test[i,])){
+	if(pred_norm[i,1] == pred_norm[i,2]){
 		score_norm <- score_norm +1
 	}
 }
 score_norm <- 1- score_norm/dim(pred)[1]
 
 #Saving data into lists
-configs <- rbind(configs,data.frame(hidden_rbm=hidden_rbm, numempochs_rbm = numepochs_rbm, batchsize_rbm=batchsize_rbm, lr_rbm=learningrate_rbm, lr_scale_rbm=learningrate_scale_rbm, cd=cd, hidden_nn=paste(hidden_nn,collapse=" "), lr_nn=learningrate_nn, lr_scale_nn=learningrate_scale_nn, numepochs_nn=numepochs_nn, batchsize_nn=batchsize_nn, using_rbm=use_rbm, err_score=score, err_score_norm = score_norm))
+configs <- rbind(configs,data.frame(hidden_rbm=hidden_rbm, numempochs_rbm = numepochs_rbm, batchsize_rbm=batchsize_rbm, lr_rbm=learningrate_rbm, lr_scale_rbm=learningrate_scale_rbm, cd=cd, hidden_nn=paste(hidden_nn,collapse=" "), lr_nn=learningrate_nn, lr_scale_nn=learningrate_scale_nn, numepochs_nn=numepochs_nn, batchsize_nn=batchsize_nn, using_rbm=use_rbm, err_score_norm = score_norm))
 predict_list[[dim(configs)[1]]] <- pred
 predict_norm_list[[dim(configs)[1]]] <- pred_norm
 nn_list[[dim(configs)[1]]] <- nn
